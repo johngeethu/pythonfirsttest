2e86b25b80f89de9db9e40208787b5bb0a17f019
gsheludkar <geethusheludkar@maximus.com>
2020-10-21 13:23:53 -0700

    Added function and test for multiplication

--- test/Firsttest.py
+++ test/Firsttest.py
@@ -6,9 +6,15 @@ def add(x):
 def subtract(x):
     return x - 1
 
+def multiply(x,y):
+    return x * y
+
 
 def test_add():
     assert add(3) == 4
 
 def test_subtract():
     assert subtract(9) == 8
+
+def test_multiply():
+    assert multiply(3,4) == 12
